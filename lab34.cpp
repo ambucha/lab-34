568f024702044c848838ba53f04741b72d1363c7
ambucha <ambuchatskiy@gmail.com>
2025-11-23 00:37:01 -0800

    step 1 complete(looks like i hjad accidnetlaly deleted the commit on the first time submitting this

--- lab34.cpp
+++ lab34.cpp
@@ -3,7 +3,7 @@
 #include <queue>
 using namespace std;
 
-const int SIZE = 7;
+const int SIZE = 13;
 
 struct Edge {
     int src, dest, weight;
@@ -96,7 +96,9 @@ int main() {
     // Creates a vector of graph edges/weights
     vector<Edge> edges = {
         // (x, y, w) —> edge from x to y having weight w
-        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,6,2},{5,6,6},{4,5,9},{2,4,4},{2,5,5}
+        // deleted nodes 1 and 6
+        // adding six new nodes, 7 8 9 10 11 12
+        {0,2,31},{0,3,19},{2,3,6},{4,5,13},{2,4,3},{2,5,5},{5,7,12},{4,7,2},{7,8,13},{7,9,7},{8,9,1},{8,10,13},{7,10,2},{10,11,11},{8,11,5},{11,12,8},{7,12,15}
     };
 
     // Creates graph
